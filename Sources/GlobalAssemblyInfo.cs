aad5d37a8527fbe55f94f11585af4a81fa71a7a0
NikGovorov <nikita.govorov@gmail.com>
2011-07-22 18:27:59 +0400

    Base project structure.

---
+++ Sources/GlobalAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Taijutsu")]
+[assembly: AssemblyDescription("Taijutsu is intended to help developers to design domain model layer.")]
+[assembly: AssemblyCopyright("Copyright 2009-2011 Nikita Govorov - All rights reserved.")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0.0")]
+
