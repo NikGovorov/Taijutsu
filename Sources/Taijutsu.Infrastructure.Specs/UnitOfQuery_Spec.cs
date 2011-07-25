f1875f3d91be6ba73bbc3f4348443b757d5af9be
NikGovorov <nikita.govorov@gmail.com>
2011-07-25 17:52:39 +0400

    clean up.

--- Sources/Taijutsu.Infrastructure.Specs/UnitOfQuery_Spec.cs
+++ Sources/Taijutsu.Infrastructure.Specs/UnitOfQuery_Spec.cs
@@ -67,7 +67,6 @@ namespace Taijutsu.Infrastructure.Specs
         [ExpectedException(typeof(QueryNotFoundException))]
         public virtual void All_delegating_methods_should_be_delegated()
         {
-            var customer = new Customer(new FullName("Test", "Test"));
             var provider = MockRepository.GenerateMock<ReadOnlyDataProvider>();
             try
             {
