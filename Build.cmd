6e22db5884c7fe70d0a298cf0b3d34958dd6db06
NikGovorov <nikita.govorov@gmail.com>
2013-08-27 00:10:50 +1000

    Update nuget to 2.7.

--- Build.cmd
+++ Build.cmd
@@ -5,5 +5,5 @@ if "%key%" == "" (
 )
 mkdir Output
 mkdir Packages
-.\Sources\.nuget\nuget.exe install .\Sources\.nuget\packages.config -solutionDir .\
+.\Sources\.nuget\nuget.exe restore .\Sources\Taijutsu.sln
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Taijutsu.proj /p:NugetKey=%key% /v:M /fl /flp:LogFile=Output\msbuild.log;Verbosity=Normal /nr:false
